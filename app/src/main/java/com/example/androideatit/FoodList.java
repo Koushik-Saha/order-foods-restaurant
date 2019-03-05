b565cb266a807cdb16ac502f1a4108706af44318
Koushik Saha <Koushik.saha666@gmail.com>
2019-03-05 10:14:41 +0600

    Show Details of Food

--- app/src/main/java/com/example/androideatit/FoodList.java
+++ app/src/main/java/com/example/androideatit/FoodList.java
@@ -1,5 +1,6 @@
 package com.example.androideatit;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -69,7 +70,10 @@ public class FoodList extends AppCompatActivity {
                 viewHolder.setItemClickListener(new ItemClickListener() {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
-                        Toast.makeText(FoodList.this, ""+local.getName(), Toast.LENGTH_SHORT).show();
+                        //Start new Activity
+                        Intent foodDetail = new Intent(FoodList.this, FoodDetail.class);
+                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
+                        startActivity(foodDetail);
                     }
                 });
             }
